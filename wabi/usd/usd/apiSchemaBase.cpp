5b90e49ed945b19d647f1b43aa17ee7a5e21dcbe
Furby™ <tyler@tylerfurby.com>
2021-06-13 01:40:43 -0600

    Test UsdGenSchema on implementation files.

--- wabi/usd/usd/apiSchemaBase.cpp
+++ wabi/usd/usd/apiSchemaBase.cpp
@@ -1,36 +1,45 @@
-//
-// Copyright 2016 Pixar
-//
-// Licensed under the Apache License, Version 2.0 (the "Apache License")
-// with the following modification; you may not use this file except in
-// compliance with the Apache License and the following modification to it:
-// Section 6. Trademarks. is deleted and replaced with:
-//
-// 6. Trademarks. This License does not grant permission to use the trade
-//    names, trademarks, service marks, or product names of the Licensor
-//    and its affiliates, except as required to comply with Section 4(c) of
-//    the License and to reproduce the content of the NOTICE file.
-//
-// You may obtain a copy of the Apache License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the Apache License with the above modification is
-// distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-// KIND, either express or implied. See the Apache License for the specific
-// language governing permissions and limitations under the Apache License.
-//
-#include "wabi/usd/usd/apiSchemaBase.h"
+/*
+ * Copyright 2021 Pixar. All Rights Reserved.
+ *
+ * Portions of this file are derived from original work by Pixar
+ * distributed with Universal Scene Description, a project of the
+ * Academy Software Foundation (ASWF). https://www.aswf.io/
+ *
+ * Licensed under the Apache License, Version 2.0 (the "Apache License")
+ * with the following modification; you may not use this file except in
+ * compliance with the Apache License and the following modification:
+ * Section 6. Trademarks. is deleted and replaced with:
+ *
+ * 6. Trademarks. This License does not grant permission to use the trade
+ *    names, trademarks, service marks, or product names of the Licensor
+ *    and its affiliates, except as required to comply with Section 4(c)
+ *    of the License and to reproduce the content of the NOTICE file.
+ *
+ * You may obtain a copy of the Apache License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the Apache License with the above modification is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+ * ANY KIND, either express or implied. See the Apache License for the
+ * specific language governing permissions and limitations under the
+ * Apache License.
+ *
+ * Modifications copyright (C) 2020-2021 Wabi.
+ */
+
 #include "wabi/usd/usd/schemaRegistry.h"
 #include "wabi/usd/usd/typed.h"
 
 #include "wabi/usd/sdf/assetPath.h"
 #include "wabi/usd/sdf/types.h"
+#include "wabi/usd/usd/apiSchemaBase.h"
 
 WABI_NAMESPACE_BEGIN
 
-// Register the schema with the TfType system.
+/**
+ * Register the schema with the TfType system. */
 TF_REGISTRY_FUNCTION(TfType)
 {
   TfType::Define<UsdAPISchemaBase, TfType::Bases<UsdSchemaBase>>();
@@ -86,14 +95,15 @@ const TfTokenVector &UsdAPISchemaBase::GetSchemaAttributeNames(bool includeInher
 
 WABI_NAMESPACE_END
 
-// ===================================================================== //
-// Feel free to add custom code below this line. It will be preserved by
-// the code generator.
-//
-// Just remember to wrap code in the appropriate delimiters:
-// 'WABI_NAMESPACE_BEGIN', 'WABI_NAMESPACE_END'.
-// ===================================================================== //
-// --(BEGIN CUSTOM CODE)--
+/**
+ * ======================================================================
+ *   Feel free to add custom code below this line. It will be preserved
+ *   by the code generator.
+ *
+ *   Just remember to wrap code in the appropriate delimiters:
+ *     - 'WABI_NAMESPACE_BEGIN', 'WABI_NAMESPACE_END'.
+ * ======================================================================
+ * --(BEGIN CUSTOM CODE)-- */
 
 #include "wabi/usd/usd/tokens.h"
 
@@ -105,9 +115,10 @@ bool UsdAPISchemaBase::_IsCompatible() const
   if (!UsdSchemaBase::_IsCompatible())
     return false;
 
-  // This virtual function call tells us whether we're an applied
-  // API schema. For applied API schemas, we'd like to check whether
-  // the API schema has been applied properly on the prim.
+  /**
+   * This virtual function call tells us whether we're an applied
+   * API schema. For applied API schemas, we'd like to check whether
+   * the API schema has been applied properly on the prim. */
   if (IsAppliedAPISchema() &&
       !GetPrim()._HasAPI(_GetTfType(), /*validateSchemaType*/ false, _instanceName)) {
     return false;
