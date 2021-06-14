0d3616a3f4f3c40070e94c9a710e9e3422711205
lvxejay <jared@onelvxe.com>
2021-06-14 01:30:48 -0500

    Cleanup build_files

--- build_files/scripts/windows/install_forge.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-@REM xcopy /s "%WABI_ROOT%\deploy\blender\barnold" "%USERPROFILE%\AppData\Roaming\Blender Foundation\Blender\2.91\scripts\addons\barnold" /Y
-@REM xcopy /s "%WABI_ROOT%\deploy\modules\AiDisplay\AiDisplay.pyd" "%BTOA_PATH%\lib\" /Y
-@REM xcopy /s "%WABI_ROOT%\deploy\modules\AiZApi" "%BTOA_PATH%\AiZApi\"  /Y
-@REM xcopy /s "%WABI_ROOT%\deploy\modules\AiZ" "%BTOA_PATH%\AiZ\" /Y
-@REM xcopy /s "%WABI_ROOT%\arnold-blender\barnold\render\lib\AiGL.pyd" "%USERPROFILE%\AppData\Roaming\Blender Foundation\Blender\2.91\scripts\addons\modules\" /Y
