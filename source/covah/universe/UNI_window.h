044a1ce2117029a9fdf3be24ddfbcb3736dce74d
Furby™ <tyler@tylerfurby.com>
2021-06-17 02:46:07 -0600

    Make wmWindow sophisticated

--- source/covah/universe/UNI_window.h
+++ source/covah/universe/UNI_window.h
@@ -16,11 +16,15 @@
  * Copyright 2021, Wabi.
  */
 
+#pragma once
+
 /**
  * @file
  * Universe.
  * Set the Stage.
  */
+
+#include "UNI_context.h"
 
 #include <wabi/wabi.h>
 
@@ -31,38 +35,83 @@
 #include <wabi/usd/usd/collectionAPI.h>
 #include <wabi/usd/usdUI/window.h>
 
-struct Scene;
+#define COVAH_DEFAULT_WINDOW_PATH "/Covah/Windows/MainWindow"
+#define COVAH_DEFAULT_WORKSPACE_PATH "Workspaces/Layout"
+#define COVAH_DEFAULT_SCREEN_PATH "Workspaces/Layout/Screen"
 
-struct wmWindow : public wabi::UsdUIWindow {
-  /** Anchor system backend pointer. */
-  void *anchorwin;
+WABI_NAMESPACE_BEGIN
 
-  wabi::UsdAttribute title;
-  wabi::UsdAttribute icon;
-  wabi::UsdAttribute state;
-  wabi::UsdAttribute dpi;
-  wabi::UsdAttribute dpifac;
-  wabi::UsdAttribute widgetunit;
-  wabi::UsdAttribute scale;
-  wabi::UsdAttribute linewidth;
-  wabi::UsdAttribute pixelsz;
-  wabi::UsdAttribute cursor;
-  wabi::UsdAttribute pos;
-  wabi::UsdAttribute size;
-  wabi::UsdAttribute type;
-  wabi::UsdAttribute workspace;
+struct wmWindow : public UsdUIWindow {
 
-  wabi::UsdAttribute align;
-  wabi::UsdRelationship screen;
+  SdfPath winhash;
+  wmWindow *parent;
+
+  UsdAttribute title;
+  UsdAttribute icon;
+  UsdAttribute state;
+  UsdAttribute dpi;
+  UsdAttribute dpifac;
+  UsdAttribute widgetunit;
+  UsdAttribute scale;
+  UsdAttribute linewidth;
+  UsdAttribute pixelsz;
+  UsdAttribute cursor;
+  UsdAttribute pos;
+  UsdAttribute alignment;
+  UsdAttribute size;
+  UsdAttribute type;
+
+  UsdUIWorkspace workspace;
+  UsdUIScreen screen;
 
   /** Active scene for this window. */
-  wabi::TfToken scene;
+  TfToken scene;
 
   /** Active session layer display name. */
   char view_layer_name[64];
+
+  /** Anchor system backend pointer. */
+  void *anchorwin;
+
+  inline wmWindow(const SdfPath &path = SdfPath(COVAH_DEFAULT_WINDOW_PATH),
+                  const SdfPath &wspace = SdfPath(COVAH_DEFAULT_WORKSPACE_PATH),
+                  const SdfPath &screen = SdfPath(COVAH_DEFAULT_SCREEN_PATH));
+
+ private:
+  SdfPath m_sdf_wspace;
+  SdfPath m_sdf_screen;
 };
+
+/**
+ * Note: A workspace and a screen
+ * must always exist on a Window,
+ * therefore, we force the appends
+ * on a wmWindow */
+
+wmWindow::wmWindow(const SdfPath &path, const SdfPath &wspace, const SdfPath &screen)
+  : UsdUIWindow(UNI.stage->DefinePrim(path)),
+    winhash(path),
+    title(CreateTitleAttr()),
+    icon(CreateIconAttr()),
+    state(CreateStateAttr()),
+    dpi(CreateDpiAttr()),
+    dpifac(CreateDpifacAttr()),
+    widgetunit(CreateWidgetunitAttr()),
+    scale(CreateScaleAttr()),
+    linewidth(CreateLinewidthAttr()),
+    pixelsz(CreatePixelszAttr()),
+    cursor(CreateCursorAttr()),
+    pos(CreatePosAttr()),
+    alignment(CreateAlignmentAttr()),
+    size(CreateSizeAttr()),
+    type(CreateTypeAttr()),
+    workspace(UsdUIWorkspace::Define(UNI.stage, path.AppendPath(wspace))),
+    screen(UsdUIScreen::Define(UNI.stage, path.AppendPath(screen)))
+{}
 
 struct wmWindowManager {
   /** All windows this manager controls. */
-  wabi::TfHashMap<wabi::TfToken, wmWindow *, wabi::TfHash> windows;
+  TfHashMap<SdfPath, wmWindow *, SdfPath::Hash> windows;
 };
+
+WABI_NAMESPACE_END
