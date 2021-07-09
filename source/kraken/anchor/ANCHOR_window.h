667e7414e7a66bc9c6fe84c3c24351f5fb36b806
furby™ <tyler@tylerfurby.com>
2021-07-09 17:37:41 -0600

    Anchor, ensure current context for validity

--- source/kraken/anchor/ANCHOR_window.h
+++ source/kraken/anchor/ANCHOR_window.h
@@ -201,7 +201,7 @@ class ANCHOR_SystemWindow : public ANCHOR_ISystemWindow
    * @return The validity of the window. */
   virtual bool getValid() const
   {
-    return true;
+    return (ANCHOR::GetCurrentContext() != NULL);
   }
 
   /**
