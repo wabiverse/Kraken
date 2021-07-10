75a1faf0aedf03753a1bb5cf7946ab5af592b7c1
furby™ <tyler@tylerfurby.com>
2021-07-10 17:56:21 -0600

    Fix HgiVulkan selected GPU formatting

--- wabi/imaging/hgiVulkan/capabilities.cpp
+++ wabi/imaging/hgiVulkan/capabilities.cpp
@@ -79,7 +79,7 @@ HgiVulkanCapabilities::HgiVulkanCapabilities(HgiVulkanDevice *device)
 
   if (HgiVulkanIsDebugEnabled())
   {
-    TF_WARN("Selected GPU %s", vkDeviceProperties.deviceName);
+    TF_WARN("Selected GPU %s\n", vkDeviceProperties.deviceName);
   }
 }
 
