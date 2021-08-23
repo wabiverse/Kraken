db5e1b35c5e476517730a3fd694e82028d5825c9
furby™ <tyler@tylerfurby.com>
2021-08-23 07:23:27 -0600

    Cleanup (Cmake now autogenerates all of this.)

--- build_files/cmake/platform/microsoft_package.cmake
+++ build_files/cmake/platform/microsoft_package.cmake
@@ -77,38 +77,6 @@ configure_file(
   ${CMAKE_BINARY_DIR}/source/creator/packages.config
   @ONLY)
 
-configure_file(
-  ${CMAKE_SOURCE_DIR}/source/creator/microsoft/App.xaml
-  ${CMAKE_BINARY_DIR}/source/creator/App.xaml
-  @ONLY)
-
-configure_file(
-  ${CMAKE_SOURCE_DIR}/source/creator/microsoft/MainWindow.xaml
-  ${CMAKE_BINARY_DIR}/source/creator/MainWindow.xaml
-  @ONLY)
-
-configure_file(
-  ${CMAKE_SOURCE_DIR}/source/creator/microsoft/App.idl
-  ${CMAKE_BINARY_DIR}/source/creator/App.idl
-  @ONLY)
-
-configure_file(
-  ${CMAKE_SOURCE_DIR}/source/creator/microsoft/MainWindow.idl
-  ${CMAKE_BINARY_DIR}/source/creator/MainWindow.idl
-  @ONLY)
-
-configure_file(
-  ${CMAKE_SOURCE_DIR}/source/creator/microsoft/pch.h
-  ${CMAKE_BINARY_DIR}/source/creator/pch.h
-  @ONLY
-)
-
-configure_file(
-  ${CMAKE_SOURCE_DIR}/source/creator/microsoft/pch.cpp
-  ${CMAKE_BINARY_DIR}/source/creator/pch.cpp
-  @ONLY
-)
-
 # Resource Paths Assets.
 set(KRAKEN_RESOURCE_RC ${CMAKE_SOURCE_DIR}/release/windows/icons/winkraken.rc)
 list(APPEND STRING_FILES
@@ -118,25 +86,6 @@ list(APPEND STRING_FILES
 # Application Manifest & Nuget Dependencies.
 set(KRAKEN_APPX_MANIFEST ${CMAKE_BINARY_DIR}/source/creator/Package.appxmanifest)
 set(KRAKEN_PACKAGES_CONFIG ${CMAKE_BINARY_DIR}/source/creator/packages.config)
-
-# XAML to define WinRT runtime typing & MIDL for language projection.
-set(KRAKEN_APPLICATION_DEFINITION_XAML ${CMAKE_BINARY_DIR}/source/creator/App.xaml)
-set(KRAKEN_APPLICATION_DEFINITION_MIDL ${CMAKE_BINARY_DIR}/source/creator/App.idl)
-set(KRAKEN_MAIN_WINDOW_XAML ${CMAKE_BINARY_DIR}/source/creator/MainWindow.xaml)
-set(KRAKEN_MAIN_WINDOW_MIDL ${CMAKE_BINARY_DIR}/source/creator/MainWindow.idl)
-
-set(KRAKEN_XAML_METADATA_PROVIDER_MIDL
-  "${CMAKE_BINARY_DIR}/source/creator/Generated Files/XamlMetaDataProvider.idl"
-)
-
-list(APPEND CONTENT_FILES
-  ${KRAKEN_APPX_MANIFEST}
-  ${KRAKEN_PACKAGES_CONFIG}
-  ${KRAKEN_APPLICATION_DEFINITION_XAML}
-  ${KRAKEN_APPLICATION_DEFINITION_MIDL}
-  ${KRAKEN_MAIN_WINDOW_XAML}
-  ${KRAKEN_MAIN_WINDOW_MIDL}
-)
 
 file(GLOB out_inst_dll "${CMAKE_BINARY_DIR}/bin/Release/*.dll")
 foreach(dlls ${out_inst_dll})
@@ -162,61 +111,9 @@ foreach(apx ${out_inst})
   list(APPEND ASSET_FILES ${CMAKE_BINARY_DIR}/source/creator/assets/${ff})
 endforeach()
 
-# set_property(SOURCE
-#   ${CONTENT_FILES}
-#   PROPERTY VS_DEPLOYMENT_CONTENT 1)
-
-# set_property(SOURCE
-#   ${ASSET_FILES}
-#   PROPERTY VS_DEPLOYMENT_CONTENT 1)
-
-# set_property(SOURCE
-#   ${STRING_FILES}
-#   PROPERTY VS_TOOL_OVERRIDE "PRIResource")
-
-# set_property(SOURCE
-#   ${ASSET_FILES}
-#   PROPERTY VS_DEPLOYMENT_LOCATION "${CMAKE_CURRENT_BINARY_DIR}/source/creator")
-
-# set_property(SOURCE
-#   ${CONTENT_FILES}
-#   PROPERTY VS_DEPLOYMENT_LOCATION "${CMAKE_CURRENT_BINARY_DIR}/source/creator")
-
-# if(KRAKEN_RELEASE_MODE)
-#   set_property(SOURCE ${RELEASE_CONTENT_FILES} PROPERTY VS_DEPLOYMENT_CONTENT 1)
-# else()
-#   set_property(SOURCE ${DEBUG_CONTENT_FILES} PROPERTY VS_DEPLOYMENT_CONTENT 1)
-# endif()
-
 # This will import nuget packages given
 # a relative-to-binary path to a project
 # & automatically fills in the required
 # XXX.vcxproj.user
 kraken_import_nuget_packages("source/creator/kraken")
-# kraken_add_midlrt_references("source/creator/kraken")
-
 kraken_import_nuget_packages("maelstrom")
-
-# set(KRAKEN_WINRT_COMPILED_APP "")
-# set_source_files_properties(${KRAKEN_WINRT_COMPILED_APP}
-#   PROPERTIES GENERATED TRUE
-# )
-# kraken_winrt_compiler("App"
-#   ${KRAKEN_APPLICATION_DEFINITION_MIDL}
-#   ${KRAKEN_APPLICATION_DEFINITION_XAML}
-# )
-
-# set(KRAKEN_WINRT_COMPILED_MAINWINDOW "")
-# set_source_files_properties(${KRAKEN_WINRT_COMPILED_MAINWINDOW}
-#   PROPERTIES GENERATED TRUE
-# )
-# kraken_winrt_compiler("MainWindow"
-#   ${KRAKEN_MAIN_WINDOW_MIDL}
-#   ${KRAKEN_MAIN_WINDOW_XAML}
-# )
-
-# set(KRAKEN_WINRT_COMPILED_METADATA "")
-# kraken_winrt_compiler("XamlMetaDataProvider"
-#   ${KRAKEN_XAML_METADATA_PROVIDER_MIDL}
-#   ${KRAKEN_XAML_METADATA_PROVIDER_MIDL}
-# )
