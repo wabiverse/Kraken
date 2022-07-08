f6d76d1eb8babef37a91e3b7f1cdef6152536630
furby™ <devs@wabi.foundation>
2022-07-07 21:04:30 -0500

    Add doc to our new macOS Swift Anchor System.

--- source/kraken/anchor/backend/backend_apple.swift
+++ source/kraken/anchor/backend/backend_apple.swift
@@ -1,6 +1,14 @@
 /**
  *  backend_apple.swift
- *  Anchor Interface for Kraken on macOS.
+ *  Anchor System for Kraken on macOS.
+ *
+ *  The interface gets auto-generated into
+ *  Anchor.h and the Anchor backend for Cocoa
+ *  calls directly into this swift class.
+ *
+ *  This system is in charge of managing the
+ *  underlying system time, displays & window
+ *  events, and other Operating System bits.
  *
  *  Created by Wabi Animation Studios
  *  Copyright © 2022 Wabi. All rights reserved.
