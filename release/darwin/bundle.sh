2943af4ec086f45c71c8bc2b38405ca2386d60c8
furby™ <devs@wabi.foundation>
2022-08-19 05:37:08 -0500

    Bump macOS .dmg from 1.725 GB -> 1.82 GB.

--- release/darwin/bundle.sh
+++ release/darwin/bundle.sh
@@ -116,8 +116,8 @@ echo
 
 # Create the disk image.
 _directory_size=$(du -sh ${_tmp_dir} | awk -F'[^0-9]*' '$0=$1')
-# 1725 is as small as kraken can go here...
-_image_size=$(echo "${_directory_size}" + 1725 | bc) # extra 1725 need for codesign to work (why on earth?)
+# 1820 is as small as kraken can go here...
+_image_size=$(echo "${_directory_size}" + 1820 | bc) # extra 1820 need for codesign to work (why on earth?)
 
 echo
 echo -n "Creating disk image of size ${_image_size}M.."
