4d205575343abf82efabc6f953a8433e9c88f7f0
furby™ <devs@wabi.foundation>
2022-09-11 02:04:39 -0500

    For devs - use MSVC community on windows.

--- build_files/scripts/windows/create_environment.cmd
+++ build_files/scripts/windows/create_environment.cmd
@@ -1,4 +1,4 @@
 @echo off
 if not defined DevEnvDir (
-    call "C:\Program Files\Microsoft Visual Studio\2022\Professional\VC\Auxiliary\Build\vcvarsall.bat" x64
+    call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 )
