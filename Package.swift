623115b0abdb87f2a2d509edcd3a33c535773a18
furby™ <devs@wabi.foundation>
2023-07-29 02:57:52 -0500

    Begin PixarUSD for swift.

--- Package.swift
+++ Package.swift
@@ -2,9 +2,9 @@
 import PackageDescription
 
 #if DEBUG
-  let pixarHeadersPath = "../build_darwin_release/bin/debug/include"
+  let pixarHeadersPath = "../build_darwin_debug/bin/include"
 #else
-  let pixarHeadersPath = "../build_darwin_release/bin/release/include"
+  let pixarHeadersPath = "../build_darwin_release/bin/include"
 #endif
 
 // ----------------------------------------------------------
@@ -15,8 +15,8 @@ let package = Package(
 
   // --- 📦 Package Products. ---
   products: [
-    .library(name: "Pixar",
-             targets: ["Pixar"]),
+    .library(name: "PixarUSD",
+             targets: ["PixarUSD"]),
 
     .executable(name: "Kraken",
                 targets: ["Kraken"]),
@@ -27,7 +27,7 @@ let package = Package(
     // -----------------------------------------------------------
     // :: :  🦄 Pixar - Universal Scene Description(USD)  :     ::
     // -----------------------------------------------------------
-    .target(name: "Pixar",
+    .target(name: "PixarUSD",
             path: ".",
             exclude: [
               "./.build",
@@ -41,15 +41,80 @@ let package = Package(
               "./source",
               "./test",
               "./Tests",
+              "./wabi/base/js/rapidjson/msinttypes",
+              "./wabi/base/gf/range.template.cpp",
+              "./wabi/base/gf/matrix.template.cpp",
+              "./wabi/base/gf/matrix2.template.cpp",
+              "./wabi/base/gf/matrix3.template.cpp",
+              "./wabi/base/gf/matrix4.template.cpp",
+              "./wabi/base/gf/wrapMatrix.template.cpp",
+              "./wabi/base/gf/wrapMatrix2.template.cpp",
+              "./wabi/base/gf/wrapMatrix3.template.cpp",
+              "./wabi/base/gf/wrapMatrix4.template.cpp",
+              "./wabi/base/gf/vec.template.cpp",
             ],
             publicHeadersPath: "wabi",
-            swiftSettings: [.interoperabilityMode(.Cxx)]),
+            cxxSettings: [
+              .unsafeFlags([
+                "-I\(pixarHeadersPath)",
+                "-I../lib/apple_darwin_arm64/python/include/python3.10",
+                "-I/opt/homebrew/include"
+              ]),
+              .define("WITH_PYTHON", to: "1"),
+              .define("WITH_METAL", to: "1"),
+              .define("WITH_USD", to: "1"),
+              .define("WITH_TBB", to: "1"),
+              .define("WITH_BOOST", to: "1"),
+              .define("WITH_PIXAR_USDVIEW", to: "1")
+            ],
+            swiftSettings: [
+              .interoperabilityMode(.Cxx),
+              .unsafeFlags([
+                "-I\(pixarHeadersPath)",
+                "-I../lib/apple_darwin_arm64/python/include/python3.10",
+                "-I/opt/homebrew/include"
+              ]),
+              .define("WITH_PYTHON"),
+              .define("WITH_METAL"),
+              .define("WITH_USD"),
+              .define("WITH_TBB"),
+              .define("WITH_BOOST"),
+              .define("WITH_PIXAR_USDVIEW")
+            ]),
 
     // ---------------------------------------------------------
     // :: :  🐙 Kraken - The Animation Platform Suite.  :     ::
     // ---------------------------------------------------------
     .executableTarget(name: "Kraken",
                       path: "source/creator",
-                      swiftSettings: [.interoperabilityMode(.Cxx)]),
-  ]
+            cxxSettings: [
+              .unsafeFlags([
+                "-I\(pixarHeadersPath)",
+                "-I../lib/apple_darwin_arm64/python/include/python3.10",
+                "-I/opt/homebrew/include",
+              ]),
+              .define("WITH_PYTHON", to: "1"),
+              .define("WITH_METAL", to: "1"),
+              .define("WITH_USD", to: "1"),
+              .define("WITH_TBB", to: "1"),
+              .define("WITH_BOOST", to: "1"),
+              .define("WITH_PIXAR_USDVIEW", to: "1"),
+            ],
+            swiftSettings: [
+              .interoperabilityMode(.Cxx),
+              .unsafeFlags([
+                "-I\(pixarHeadersPath)",
+                "-I../lib/apple_darwin_arm64/python/include/python3.10",
+                "-I/opt/homebrew/include",
+              ]),
+              .define("WITH_PYTHON"),
+              .define("WITH_METAL"),
+              .define("WITH_USD"),
+              .define("WITH_TBB"),
+              .define("WITH_BOOST"),
+              .define("WITH_PIXAR_USDVIEW"),
+            ]),
+  ],
+  cLanguageStandard: .c90,
+  cxxLanguageStandard: .cxx20
 )
