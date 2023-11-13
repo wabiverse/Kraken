7c6016b317504da421a835ee32fb467dff567ffb
furby™ <devs@wabi.foundation>
2023-11-12 18:43:16 -0600

    Bump Pixar USD.

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
 
   // --- 🦄 Package Dependencies. ---
   dependencies: [
-    .package(url: "https://github.com/wabiverse/SwiftUSD.git", from: "23.8.0"),
+    .package(url: "https://github.com/wabiverse/SwiftUSD.git", from: "23.8.1"),
   ],
 
   // --- 🎯 Package Targets. ---
