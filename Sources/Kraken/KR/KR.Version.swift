78839955e1780c74e688a06ad223db00b9a18c2a
furby™ <devs@wabi.foundation>
2024-05-04 21:25:24 -0600

    Release Kraken v1.0.9.

--- Sources/Kraken/KR/KR.Version.swift
+++ Sources/Kraken/KR/KR.Version.swift
@@ -31,7 +31,7 @@ public extension Kraken
 {
   /** 
    * The current version of Kraken. */
-  static let version = ".".join(array: Pixar.GfVec3i(1, 0, 7))
+  static let version = ".".join(array: Pixar.GfVec3i(1, 0, 9))
 
   /** 
    * Returns version information for Kraken and USD. */
